d3bfe337dde01c0b723161fdd8da6953519fe1d4
Andrey Sidorov <takandar@gmail.com>
2024-05-29 01:41:14 +0300

    better logging

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,30 @@ mod ui;
 mod utils;
 mod walker;
 
-use std::env;
-
 use eframe::run_native;
 use crate::ui::App;
 use log::info;
+use std::env;
+use env_logger::{Builder, fmt::Color};
+use std::io::Write;
 
 fn main() {
+    // Set the RUST_LOG environment variable programmatically
     env::set_var("RUST_LOG", "info");
-    env_logger::init();
+
+    // Initialize env_logger with a custom format
+    Builder::from_default_env().format(|buf, record| {
+        let mut style = buf.style();
+        style.set_color(Color::Green);
+
+        writeln!(
+            buf,
+            "[{}] {}",
+            style.value(record.target()),  // Only the module name is green
+            record.args()
+        )
+    }).init();
+
     info!("Starting File Tree Viewer...");
     let options = eframe::NativeOptions::default();
     run_native(Box::new(App::new()), options);
